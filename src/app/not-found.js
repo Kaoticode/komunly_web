c375cb26e3307fef4f4b4d568ee1cf12b1675a81
Wilberk Ledezma <ledezma.wilberk@gmail.com>
2024-09-09 13:32:36 -0400

    corrección en los estilos

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,15 +1,20 @@
-import Navbar from '@/components/Navbar'
-import Link from 'next/link'
+import Navbar from "@/components/Navbar";
+import Link from "next/link";
 
 export default function NotFound() {
-    return (
-        <div className='h-full w-full'>
-            <Navbar/>
-            <div className='h-screen flex flex-col justify-center items-center gap-7'>
-            <h1 className='text-3xl'>Página no encontrada – 404!</h1>
-            <Link href="/" className="outline outline-2 outline-gray-500 px-5 py-2 rounded-3xl hover:bg-gray-400/10 transition-all hover:outline-white hover:outline-2" > Vuelve al inicio </Link>
-            </div>
-        </div>
-    )
-
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <div className="h-screen flex flex-col justify-center items-center gap-7">
+        <h1 className="text-3xl">Página no encontrada – 404!</h1>
+        <Link
+          href="/"
+          className="outline outline-2 outline-komunly-gray-light px-5 py-2 rounded-3xl hover:bg-komunly-gray-medium transition-all hover:outline-white hover:outline-2"
+        >
+          {" "}
+          Vuelve al inicio{" "}
+        </Link>
+      </div>
+    </div>
+  );
 }
